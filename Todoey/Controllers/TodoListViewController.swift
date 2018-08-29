90921e846bc2cf3f2f2551fc1fece1b2df60cf03
Test <Test@davids-macbook-pro.local>
2018-08-29 22:03:01 +0100

    Persisting data using Codable protocol and Plist

--- Todoey/Controllers/TodoListViewController.swift
+++ Todoey/Controllers/TodoListViewController.swift
@@ -10,18 +10,16 @@ import UIKit
 
 class TodoListViewController: UITableViewController {
 
-   var itemArray = ["Find Mike", "Buy Eggos", "Destroy Demogorgon"]
+   var itemArray = [Item]()
     
-    let defaults = UserDefaults.standard
-    
+            let dataFilePath = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first?.appendingPathComponent("Items.plist")
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-        if let items = defaults.array(forKey: "TodoListArrat") as? [String] {
-            itemArray = items
-        }
+        
+        loadItems()
     }
 
+    
     //MARK - Setting up Table View and returning Cell
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return itemArray.count
@@ -29,9 +27,14 @@ class TodoListViewController: UITableViewController {
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "TodoItemCell", for: indexPath)
         
-        cell.textLabel?.text = itemArray[indexPath.row]
+        let item = itemArray[indexPath.row]
+        cell.textLabel?.text = item.title
+        
+        cell.accessoryType = item.done == true ? .checkmark : .none
+
         
         return cell
+
     }
 
     //MARK - Setting up what happens when you Select cell
@@ -39,20 +42,20 @@ class TodoListViewController: UITableViewController {
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         print(itemArray[indexPath.row])
         
+        itemArray[indexPath.row].done = !itemArray[indexPath.row].done
         
-        if tableView.cellForRow(at: indexPath)?.accessoryType == .checkmark {
-            tableView.cellForRow(at: indexPath)?.accessoryType = .none
-        } else {
-            tableView.cellForRow(at: indexPath)?.accessoryType = .checkmark
-            
-        }
+        saveItems()
         
         tableView.deselectRow(at: indexPath, animated: true)
-        
+        tableView.reloadData()
 
     }
 
-//MARK - Add New Items Popup
+    
+    
+    
+    //MARK - Add New Items Popup
+    
     @IBAction func addButtonPressed(_ sender: UIBarButtonItem) {
         
         var textField = UITextField()
@@ -62,22 +65,49 @@ class TodoListViewController: UITableViewController {
         let action = UIAlertAction(title: "Add Item", style: .default) { (action) in
             // What happens when you press add item
             
-            self.itemArray.append(textField.text!)
+            let newItem = Item()
+            newItem.title = textField.text!
             
-            self.defaults.set(self.itemArray, forKey: "TodoListArray")
+            self.itemArray.append(newItem)
             
-            self.tableView.reloadData()
-            print("Re-Load Done")
+           self.saveItems()
         }
         
         alert.addTextField { (alertTextField) in
             alertTextField.placeholder = "Create new Item"
             textField = alertTextField
+        
         }
+        
+        
         alert.addAction(action)
         
         present(alert, animated: true, completion:nil)
     }
     
+    //MARK - Model Manipulation Methods
+    
+    func saveItems() {
+        let encoder = PropertyListEncoder()
+        
+        do {
+            let data = try encoder.encode(itemArray)
+            try data.write(to: dataFilePath!)
+        } catch {
+            print("Error encoding item array, \(error)")
+        }
+        
+        self.tableView.reloadData()
+    }
+    
+    func loadItems() {
+        if let data = try? Data(contentsOf: dataFilePath!) {
+            let decoder = PropertyListDecoder()
+            do {
+            itemArray = try decoder.decode([Item].self, from: data)
+            } catch {
+               print("Error decoding item array, \(error)")
+            }
+    }
 }
-
+}
---
+++ Todoey/Data Model/Item.swift
@@ -0,0 +1,15 @@
+//
+//  Item.swift
+//  Todoey
+//
+//  Created by Test on 19/08/2018.
+//  Copyright © 2018 David Lowe. All rights reserved.
+//
+
+import Foundation
+
+class Item: Codable {
+    var title : String = ""
+    var done : Bool = false
+    
+}
